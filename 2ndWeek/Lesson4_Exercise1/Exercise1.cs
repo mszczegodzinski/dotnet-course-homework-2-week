0f8bd3f6f94eccfe5ed59a9e8d32e93f90879283
Marek Szczegodzinski <mszczegodzinski@o2.pl>
2020-08-06 01:13:33 +0200

    add exercise1 solution

--- 2ndWeek/Lesson4_Exercise1/Exercise1.cs
+++ 2ndWeek/Lesson4_Exercise1/Exercise1.cs
@@ -2,11 +2,22 @@
 
 namespace Lesson4_Exercise1
 {
+    // Stwórz program, w którym zadeklarujesz zmienne dotyczące danych pracownika firmy.Dane które chcesz przetrzymywać to:
+    //a.Imię,
+    //b.Nazwisko
+    //c.Wiek
+    //d.Płeć (‘m’ albo ‘k’)
+    //e.PESEL
+    //f.Numer pracownika (np. 2509324094)
+    //Zadeklaruj zmienne odpowiednich typów, które mogą przetrzymywać te informacje
     class Exercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Exercise1: ");
+            string name = "Marek";
+
         }
     }
 }
