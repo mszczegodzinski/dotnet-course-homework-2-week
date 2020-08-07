757bfc1494fbb3a6442325405f1de084694b3088
Marek Szczegodzinski <mszczegodzinski@o2.pl>
2020-08-07 11:05:54 +0200

    add solution to exercise 8

--- 2ndWeek/Lesson7/L7_Ex8/Ex08.cs
+++ 2ndWeek/Lesson7/L7_Ex8/Ex08.cs
@@ -6,7 +6,31 @@ namespace L7_Ex8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lesson 7 Exercise 8: ");
+            int math = 80;
+            int physics = 71;
+            int chemistry = 0;
+            Console.WriteLine(
+                "Student's results: \r\n" +
+                $"Math: {math} \r\n" +
+                $"Physics: {physics} \r\n" +
+                $"Chemistry: {chemistry} \r\n");
+            if (math > 70 || physics > 45 || chemistry > 45)
+            {
+                Console.WriteLine("Studnent may enroll");
+            }
+            else if (math + physics + chemistry > 180)
+            {
+                Console.WriteLine("Studnent may enroll");
+            }
+            else if (math > 150)
+            {
+                Console.WriteLine("Studnent may enroll");
+            }
+            else
+            {
+                Console.WriteLine("Studnent may not enroll :(");
+            }
         }
     }
 }
