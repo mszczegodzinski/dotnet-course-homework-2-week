34384dc0ba813321db84104c2a1fccac43017ad7
Marek Szczegodzinski <mszczegodzinski@o2.pl>
2020-08-07 10:36:06 +0200

    add solution to exercise 7

--- 2ndWeek/Lesson7/L7_Ex7/Ex07.cs
+++ 2ndWeek/Lesson7/L7_Ex7/Ex07.cs
@@ -6,7 +6,32 @@ namespace L7_Ex7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lesson 7 Exercise 7: ");
+            Console.WriteLine("Type three numbers: ");
+            int firstNumber;
+            int secondNumber;
+            int thirdNumber;
+            bool isFirstNumberCorrect = Int32.TryParse(Console.ReadLine(), out firstNumber);
+            bool isSecondNumberCorrect = Int32.TryParse(Console.ReadLine(), out secondNumber);
+            bool isThirdNumberCorrect = Int32.TryParse(Console.ReadLine(), out thirdNumber);
+
+            if (isFirstNumberCorrect && isSecondNumberCorrect && isThirdNumberCorrect)
+            {
+                int theBiggestNumber = firstNumber;
+                if (theBiggestNumber < secondNumber)
+                {
+                    theBiggestNumber = secondNumber;
+                }
+                if (theBiggestNumber < thirdNumber)
+                {
+                    theBiggestNumber = thirdNumber;
+                }
+                Console.WriteLine($"The biggest number from: {firstNumber}, {secondNumber}, {thirdNumber} is {theBiggestNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect inputs type. All three numbers have to be an integer");
+            }
         }
     }
 }
