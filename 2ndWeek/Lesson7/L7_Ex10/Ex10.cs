1bfd9f3f497e8c21fec51f8c762e20c5bfa6e9c5
Marek Szczegodziński <marek.szczegodzinski@wisene.pl>
2020-08-08 02:08:46 +0200

    update solution to exercise 10

--- 2ndWeek/Lesson7/L7_Ex10/Ex10.cs
+++ 2ndWeek/Lesson7/L7_Ex10/Ex10.cs
@@ -7,51 +7,37 @@ namespace L7_Ex10
         static void Main(string[] args)
         {
             Console.WriteLine("Lesson 7 Exercise 10: ");
-            int firstSide = 65;
-            int secondSide = 40;
-            int thirdSide = 55;
-            int longestSide = 0;
-            int mediumSide = 0;
-            int shortestSide = 0;
+            int firstSide;
+            int secondSide;
+            int thirdSide;
+            Console.WriteLine("Give the first side: ");
+            bool isFirstSideCorrect = Int32.TryParse(Console.ReadLine(), out firstSide);
+            Console.WriteLine("Give the second side: ");
+            bool isSecondSideCorrect = Int32.TryParse(Console.ReadLine(), out secondSide);
+            Console.WriteLine("Give the third side: ");
+            bool isThirdSideCorrect = Int32.TryParse(Console.ReadLine(), out thirdSide);
 
-            //sort sides from the shortest to the longest:
-            if (firstSide < secondSide)
+            if (isFirstSideCorrect && isSecondSideCorrect && isThirdSideCorrect)
             {
-                if (secondSide < thirdSide)
+                if (firstSide > 0 && secondSide > 0 && thirdSide > 0)
                 {
-                    shortestSide = firstSide;
-                    mediumSide = secondSide;
-                    longestSide = thirdSide;
+                    if (firstSide + secondSide > thirdSide && firstSide + thirdSide > secondSide && secondSide + thirdSide > firstSide)
+                    {
+                        Console.WriteLine($"It is possible to build a triangle from the sides: {firstSide}, {secondSide}, {thirdSide}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"It is not possible to build a triangle from the sides: {firstSide}, {secondSide}, {thirdSide}");
+                    }
                 }
-            }
-            else if (firstSide < thirdSide)
-            {
-                if (thirdSide < secondSide)
+                else
                 {
-                    shortestSide = firstSide;
-                    mediumSide = thirdSide;
-                    longestSide = secondSide;
+                    Console.WriteLine("Incorrect input data. All three sides should be greater than 0");
                 }
             }
             else
             {
-                longestSide = firstSide;
-                if (secondSide < thirdSide)
-                {
-                    shortestSide = secondSide;
-                    mediumSide = thirdSide;
-                }
-            }
-            Console.WriteLine($"{shortestSide}, {mediumSide}, {longestSide}");
-
-            //check if it's possible to build a triangle from the given sides:
-            if (longestSide < mediumSide + shortestSide)
-            {
-                Console.WriteLine($"It is possible to build a triangle from the sides: {firstSide}, {secondSide}, {thirdSide}");
-            }
-            else
-            {
-                Console.WriteLine($"It is not possible to build a triangle from the sides: {firstSide}, {secondSide}, {thirdSide}");
+                Console.WriteLine("Incorrect input data. All three sides should be integers");
             }
         }
     }
